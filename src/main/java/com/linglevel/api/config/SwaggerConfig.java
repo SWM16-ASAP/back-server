1e6654158284065c812f900e28e9ac87d4ba50e8
solfe <devsolfe@gmail.com>
2025-06-27 12:10:25 +0900

    Init: swagger setting

---
+++ src/main/java/com/linglevel/api/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.linglevel.api.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("LLV API")
+                        .description("Linglevel API 명세서")
+                        .version("1.0.0"));
+    }
+} 
