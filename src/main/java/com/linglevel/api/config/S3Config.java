a980d04c235cc7ee774e8e062dd52d2841b0d611
solfe <devsolfe@gmail.com>
2025-07-14 16:28:35 +0900

    Feat: add config with s3

---
+++ src/main/java/com/linglevel/api/config/S3Config.java
@@ -0,0 +1,36 @@
+package com.linglevel.api.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+    @Value("${aws.s3.region}")
+    private String region;
+
+    @Value("${aws.access-key}")
+    private String accessKey;
+
+    @Value("${aws.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public S3Client s3Client() {
+        var builder = S3Client.builder()
+                .region(Region.of(region));
+
+        // 로컬을 위한 명시적 인증
+        if (!accessKey.isEmpty() && !secretKey.isEmpty()) {
+            AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+            builder.credentialsProvider(StaticCredentialsProvider.create(credentials));
+        }
+
+        return builder.build();
+    }
+} 
