02e1aefd7e9c3c42283001eb76ab56619edd7dfe
solfe <devsolfe@gmail.com>
2025-07-21 16:46:25 +0900

    Feat: suggestion

--- src/main/java/com/linglevel/api/suggestions/controller/SuggestionsController.java
+++ src/main/java/com/linglevel/api/suggestions/controller/SuggestionsController.java
@@ -3,6 +3,7 @@ package com.linglevel.api.suggestions.controller;
 import com.linglevel.api.common.dto.ExceptionResponseDTO;
 import com.linglevel.api.suggestions.dto.SuggestionRequest;
 import com.linglevel.api.suggestions.dto.SuggestionResponse;
+import com.linglevel.api.suggestions.service.SuggestionsService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -10,18 +11,22 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api/v1/suggestions")
 @RequiredArgsConstructor
 @Tag(name = "Suggestions", description = "고객 건의 관련 API")
 public class SuggestionsController {
+
+    private final SuggestionsService suggestionsService;
 
     @Operation(summary = "고객 건의 제출", description = "고객의 건의사항을 제출받습니다.")
     @ApiResponses(value = {
@@ -32,7 +37,7 @@ public class SuggestionsController {
     })
     @PostMapping
     public ResponseEntity<SuggestionResponse> submitSuggestion(@RequestBody SuggestionRequest request) {
-        // TODO: 건의사항 처리 로직 구현
-        return ResponseEntity.status(HttpStatus.CREATED).body(new SuggestionResponse("Suggestion submitted successfully."));
+        SuggestionResponse response = suggestionsService.saveSuggestion(request);
+        return ResponseEntity.ok(response);
     }
 }
