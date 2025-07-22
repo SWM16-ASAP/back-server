8ff2e97439cf145cba80cc85887795767a6f4f2d
Solf <74342496+SolfE@users.noreply.github.com>
2025-07-22 13:30:53 +0900

    Fix: swagger servers setting (#31)
    * Feat: add swagger server list
    
    * Feat: change branch for CICD test
    
    * Refactor: change swagger url to use properties

--- src/main/java/com/linglevel/api/config/SwaggerConfig.java
+++ src/main/java/com/linglevel/api/config/SwaggerConfig.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,12 +16,18 @@ import java.util.List;
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${swagger.servers}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Ling Level API")
                         .version("1.0.0"))
+                .servers(List.of(
+                        new Server().url(serverUrl).description("Current Environment Server")
+                ))
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth", new SecurityScheme()
                                 .type(SecurityScheme.Type.HTTP)
