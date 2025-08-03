f95e80dcbaaa90db1bb1f14873f3ff986518b108
solfe <devsolfe@gmail.com>
2025-08-03 17:29:46 +0900

    Fix: specify redis ssl using

--- src/main/java/com/linglevel/api/config/RedisConfig.java
+++ src/main/java/com/linglevel/api/config/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -19,10 +20,23 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
+    @Value("${spring.data.redis.ssl.enabled}")
+    private boolean ssl;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
-        return new LettuceConnectionFactory(config);
+
+        LettuceClientConfiguration clientConfig;
+        if (ssl) {
+            clientConfig = LettuceClientConfiguration.builder()
+                    .useSsl()
+                    .build();
+        } else {
+            clientConfig = LettuceClientConfiguration.builder().build();
+        }
+
+        return new LettuceConnectionFactory(config, clientConfig);
     }
 
     @Bean
