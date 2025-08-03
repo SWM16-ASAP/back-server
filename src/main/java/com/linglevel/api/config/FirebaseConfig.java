465c57bb1bdc34e08a6db95f3816da058702dbde
solfe <devsolfe@gmail.com>
2025-08-03 12:22:39 +0900

    Fix: firebase config to system enviroment variable

--- src/main/java/com/linglevel/api/config/FirebaseConfig.java
+++ src/main/java/com/linglevel/api/config/FirebaseConfig.java
@@ -4,21 +4,30 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Base64;
 
 @Configuration
 public class FirebaseConfig {
+    
+    @Value("${firebase.config}")
+    private String firebaseConfig;
+    
     @Bean
     public FirebaseAuth firebaseAuth() throws IOException {
-        FileInputStream serviceAccount =
-                new FileInputStream("./src/main/resources/firebase.json");
+        byte[] decodedConfig = Base64.getDecoder().decode(firebaseConfig);
+        
+        GoogleCredentials credentials = GoogleCredentials.fromStream(
+            new ByteArrayInputStream(decodedConfig)
+        );
 
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setCredentials(credentials)
                 .build();
 
         FirebaseApp.initializeApp(options);
