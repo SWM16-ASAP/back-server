a0d0e9ae14e5c0268589ed69821221715db845fb
solfe <devsolfe@gmail.com>
2025-08-08 13:15:58 +0900

    Feat: add swagger word, bookmark api

---
+++ src/main/java/com/linglevel/api/bookmarks/dto/GetBookmarkedWordsRequest.java
@@ -0,0 +1,32 @@
+package com.linglevel.api.bookmarks.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "북마크된 단어 목록 조회 요청")
+public class GetBookmarkedWordsRequest {
+    
+    @Schema(description = "페이지 번호", 
+            example = "1", 
+            minimum = "1",
+            defaultValue = "1")
+    private Integer page = 1;
+    
+    @Schema(description = "페이지 크기", 
+            example = "10", 
+            minimum = "1", 
+            maximum = "50",
+            defaultValue = "10")
+    private Integer limit = 10;
+    
+    @Schema(description = "검색 키워드 (단어 부분 일치 검색)", 
+            example = "magn")
+    private String search;
+}
