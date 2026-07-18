92ba60e0501c3c66a79ba60435d1793253acc31a
solfe <devsolfe@gmail.com>
2026-07-18 23:37:45 +0900

    feat: add environment file storage

--- infra/performance-test/terraform/platform/iam.tf
+++ infra/performance-test/terraform/platform/iam.tf
@@ -18,3 +18,21 @@ resource "aws_iam_role_policy_attachment" "task_execution" {
   role       = aws_iam_role.task_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+data "aws_iam_policy_document" "task_execution_environment_file" {
+  statement {
+    actions   = ["s3:GetBucketLocation"]
+    resources = [aws_s3_bucket.environment_files.arn]
+  }
+
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.environment_files.arn}/${local.environment_file_key}"]
+  }
+}
+
+resource "aws_iam_role_policy" "task_execution_environment_file" {
+  name   = "environment-file-read"
+  role   = aws_iam_role.task_execution.id
+  policy = data.aws_iam_policy_document.task_execution_environment_file.json
+}
